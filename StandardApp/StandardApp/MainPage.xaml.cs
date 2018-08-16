90e28088d213292c5d3519e271bfd1b2dbab896f
Andreidg <andrei31.dg@gmail.com>
2018-08-16 10:16:41 +0300

    Add project files.

---
+++ StandardApp/StandardApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StandardApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void button_Pressed(object sender, EventArgs e)
+        {
+            Page page = (Page)Activator.CreateInstance(typeof(HelloPage));
+            //await Navigation.PushModalAsync(page);
+            await Navigation.PushAsync(page);
+        }
+
+        private async void button2_Pressed(object sender, EventArgs e)
+        {
+            Page page = (Page)Activator.CreateInstance(typeof(XamlAndCode));
+            //await Navigation.PushModalAsync(page);
+            await Navigation.PushAsync(page);
+        }
+
+        private async void button1_Pressed(object sender, EventArgs e)
+        {
+            Page page = (Page)Activator.CreateInstance(typeof(AbsoluteDemo));
+            //await Navigation.PushModalAsync(page);
+            await Navigation.PushAsync(page);
+        }
+
+        private async void button3_Pressed(object sender, EventArgs e)
+        {
+            Page page = (Page)Activator.CreateInstance(typeof(GridDemo));
+            //await Navigation.PushModalAsync(page);
+            await Navigation.PushAsync(page);
+        }
+    }
+}
---
+++ StandardApp/StandardApp/XamlAndCode.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StandardApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class XamlAndCode : ContentPage
+	{
+		public XamlAndCode ()
+		{
+			InitializeComponent ();
+		}
+        void OnSliderValueChanged(object sender,
+                                  ValueChangedEventArgs args)
+        {
+            valueLabel.Text = args.NewValue.ToString("F3");
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked!",
+                "The button labeled '" + button.Text + "' has been clicked",
+                "OK");
+        }
+    }
+}
